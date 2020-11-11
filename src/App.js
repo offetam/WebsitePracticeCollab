d94b38fd8ebca59f08d331f3224bff686a0d5ccb
etam4225 <31863364+Etam4225@users.noreply.github.com>
2020-11-10 20:20:06 -0500

    random title editing

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-	<div className = "Title">
-		<h1> Temp </h1>
-	</div>
-    
-  );
+		return (
+			<div className="Title">
+				<h1> How-Dy </h1>
+			</div>
+
+		);
+
 }
 
 export default App;
