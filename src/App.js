7f49f71b66b44fe106c042a84a20b346e9f9874b
etam4225 <31863364+Etam4225@users.noreply.github.com>
2020-11-10 19:52:55 -0500

    Removed starter code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div class = "header">Hello and welcome to the Movie Blog! </div>
-	<div className = "info">
-	<a className = "contact us" href= "Ethan Tam Main Page.html" target = "_blank">Contact Us	</a>
+	<div className = "Title">
+		<h1> Temp </h1>
 	</div>
-      <header className="App-header">
-	 <title> Movie Blog.HTML </title> 
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
     
   );
 }
